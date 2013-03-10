13f9dcefe2c102381c01ef1230c07165b1e3e867
outcoldman <outcoldman@outlook.com>
2013-03-10 10:31:50 -0700

    Add nunit packacge, Windows sqllite library

---
+++ WindowsRuntime/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OutcoldSolutions.SQLite")]
+[assembly: AssemblyDescription("SQLite Windows Runtime library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OutcoldSolutions")]
+[assembly: AssemblyProduct("OutcoldSolutions SQLite")]
+[assembly: AssemblyCopyright("\x00a9 OutcoldSolutions. All rights reserved.")]
+[assembly: AssemblyCulture("en-us")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.31003.*")]
+[assembly: AssemblyFileVersion("1.0.31003.0")]
+[assembly: CLSCompliant(false)]
